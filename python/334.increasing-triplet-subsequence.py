9cbfde7a16c9d5d4912a247a0ab61ede92f34908
Luan Arita <luan.arita@unesp.br>
2024-03-27 09:59:27 -0300

    334

--- python/334.increasing-triplet-subsequence.py
+++ python/334.increasing-triplet-subsequence.py
@@ -7,6 +7,16 @@
 # @lc code=start
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
+        s = f = float('inf')
+
+        for n in nums:
+            if n <= f:
+                f = n
+            elif n <= s:
+                s = n
+            elif n > s:
+                return True
+        return False
         
 # @lc code=end
 
